74ca4218ca55f264f9ffa4036de21dd8a42739ff
lynx1902 <davepushkar02@gmail.com>
2022-04-18 00:18:29 +0530

    added animation and results
    Co-authored-by: sakshigiri2002 <sakshigiri2002@users.noreply.github.com>
    Co-authored-by: Lovesh goyal <lovesh1711@users.noreply.github.com>
    Co-authored-by: Yaswanth <yaswanth1701@users.noreply.github.com>
    Co-authored-by: Arcane-01 <Arcane-01@users.noreply.github.com>
    Co-authored-by: Sahil Dharme <ash-S26@users.noreply.github.com>

--- taut-cable/taut_cable_dynamics.py
+++ taut-cable/taut_cable_dynamics.py
@@ -12,7 +12,7 @@ def euler_dynamics(x,v,a,t):
 
 #simulation time 
 tspan=np.arange(0,8,0.01)
-t=800
+t=800                     # number of iterations
 
 # Drone params
 J= 0.015                  #moment of inertia of UAV
@@ -30,17 +30,17 @@ theta_ddot = 0
 
 delta_t=0.01
 
-# PD controllers for alpha-(from equation uAlpha)
-kP_alpha = 6
-kD_alpha = 5
+# PD gain values for alpha-(from equation uAlpha)
+kP_alpha = 4
+kD_alpha = 3.8
 
-# PD controllers for theta and r
-kP_theta = 1
-kD_theta = 3
+# PD gain values for theta and r
+kP_theta = 4
+kD_theta = 3.5
 kD_r= 6
 kP_r= 6
 
-#arrays for storing the updating values of control objectives
+#arrays for storing the updating values of control objectives and their derivatives
 r_arr = []              
 r_d_arr = []            
 theta_arr= []           
@@ -54,11 +54,11 @@ theta = pi/15
 alpha = pi/8
 
 # desired states
-r_des=1
+r_des=2
 alpha_des = 9*pi/10
 theta_des=-pi/32
 
-# resolving the components in 2d plane
+# resolving the components in 2d plane (polar to cartesian)
 X = r_des*sin(alpha_des)
 Y = r_des*cos(alpha_des)
 X_dot = 0
@@ -95,7 +95,7 @@ for i in range(t):
     alpha_ddot = - (2*r_dot*alpha_dot + gravity*cos(alpha))/r + ((cos(theta_C + alpha))/(mass*r))*u1
     theta_ddot = u2/J
 
-#   Euler's approach for solving dynamics
+   #Using Euler's method to update the values
 
     #  for calculating r
     r_dot,r = euler_dynamics(r,r_dot,r_ddot,delta_t)
@@ -149,15 +149,15 @@ plt.show()
 # Animation code 
 def anime():
     l =   ( J * 12/ mass) * (1 / 2)                  # length of drone
-    l1=l+0.15
+    l1=l+0.15                                        
     fig = plt.figure()
     ax = fig.add_subplot(111, autoscale_on=False, aspect='equal')
     
     ax.grid()
     
-    line, = ax.plot([], [], '-', lw=5.5)
-    lined, = ax.plot([], [], '-', lw=5.5)
-    line1, = ax.plot([], [], '-', lw=3)
+    line, = ax.plot([], [], '-', lw=5.5)            # main body 
+    lined, = ax.plot([], [], '-', lw=5.5)           # desired position
+    line1, = ax.plot([], [], '-', lw=3)             #span length
     lined1, = ax.plot([], [], '-', lw=3)
     cable, = ax.plot([], [], '-', lw=1)
 
@@ -176,14 +176,18 @@ def anime():
         global T
 
         while (i < len(tspan)):
+            
+            # calculating the position using the current state matrix
             y = [solvedState[i, 1] - (l / 2) * np.cos(-solvedState[i, 2]),
                 solvedState[i, 1] + (l / 2) * np.cos(-solvedState[i, 2])]
             z = [solvedState[i, 0] - (l / 2) * np.sin(-solvedState[i, 2]),
                 solvedState[i, 0] + (l / 2) * np.sin(-solvedState[i, 2])]
 
+            # desired state
             yd = [des_state[1] - (l / 2) * np.cos(-des_state[2]), des_state[1] + (l / 2) * np.cos(-des_state[2])]
             zd = [des_state[0] - (l / 2) * np.sin(-des_state[2]), des_state[0] + (l / 2) * np.sin(-des_state[2])]
 
+            
             y1 = [solvedState[i, 1] - (l1 / 2) * np.cos(-solvedState[i, 2]),
                 solvedState[i, 1] + (l1 / 2) * np.cos(-solvedState[i, 2])]
             z1 = [solvedState[i, 0] - (l1 / 2) * np.sin(-solvedState[i, 2]),
@@ -210,7 +214,7 @@ def anime():
 
     ani = animation.FuncAnimation(fig, animate,
                                   interval = 1, blit=False, init_func=init, repeat = False, frames=t)
-    # ani.save('2D Min Accel.mp4',writer='ffmpeg',fps=30,bitrate=1800)     # to save the clip
+    # ani.save('2D Min.mp4',writer='ffmpeg',fps=30,bitrate=1800)     # to save the clip
 
     plt.show()
     return None
